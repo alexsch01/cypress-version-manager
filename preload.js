a0a76eb41b86a485804dda376e4b5a976feb75d3
Alex Schwartz <alexschwartz01@gmail.com>
2026-08-19 07:29:53 -0400

    feat: don't depend on defineConfig anymore

--- preload.js
+++ preload.js
@@ -3,7 +3,6 @@
 const Module = require('node:module');
 const path = require('node:path');
 const cp = require('node:child_process');
-const fs = require('node:fs');
 
 const originalResolveFilename = Module._resolveFilename;
 Module._resolveFilename = function (request, parent, isMain, options) {
@@ -23,62 +22,47 @@ Module._resolveFilename = function (request, parent, isMain, options) {
 
 const originalLoad = Module._load;
 Module._load = function (request, parent, isMain) {
-    if (request.endsWith('cypress.config.js')) {
-        const cypressConfigFile = fs.readFileSync(request, 'utf8');
-        if (!cypressConfigFile.includes('module.exports = defineConfig({')) {
-            throw new Error('Need to use defineConfig in cypress.config.js');
-        }
-    }
-
     const loadedModule = originalLoad.apply(this, arguments);
 
-    // If the module being required is the root 'cypress' package
-    if (request === 'cypress') {
+    if (request.endsWith('cypress.config.js')) {
         const globalCypressPath = process.env.CYPRESS_VERSION_MANAGER_GLOBAL_CYPRESS_PATH;
         const newWebpackPath = path.resolve(globalCypressPath, '@cypress/webpack-preprocessor');
 
         const wp = require(newWebpackPath);
-        const originalDefineConfig = loadedModule.defineConfig;
 
-        if (originalDefineConfig && !loadedModule._isPatched) {
-            loadedModule.defineConfig = function (userConfig = {}) {
-                const { e2e = {}, ...restConfig } = userConfig;
-                const originalSetupNodeEvents = e2e.setupNodeEvents;
+        const { e2e = {}, ...restConfig } = userConfig;
+        const originalSetupNodeEvents = e2e.setupNodeEvents;
 
-                if (restConfig.reporter === 'cypress-mochawesome-reporter') {
-                    restConfig.reporter = path.resolve(globalCypressPath, 'cypress-mochawesome-reporter');
+        if (restConfig.reporter === 'cypress-mochawesome-reporter') {
+            restConfig.reporter = path.resolve(globalCypressPath, 'cypress-mochawesome-reporter');
+        }
+
+        return {
+            ...restConfig,
+            e2e: {
+                ...e2e,
+                setupNodeEvents(on, config) {
+                    // Register the Webpack preprocessor automatically
+                    on(
+                        'file:preprocessor',
+                        wp({
+                            webpackOptions: {
+                                resolve: {
+                                    modules: [
+                                        'node_modules',
+                                        globalCypressPath,
+                                    ].filter(Boolean),
+                                },
+                            },
+                        })
+                    );
+
+                    // Run any custom setupNodeEvents logic passed by the user
+                    if (typeof originalSetupNodeEvents === 'function') {
+                        return originalSetupNodeEvents(on, config);
+                    }
                 }
-
-                return originalDefineConfig({
-                    ...restConfig,
-                    e2e: {
-                        ...e2e,
-                        setupNodeEvents(on, config) {
-                            // Register the Webpack preprocessor automatically
-                            on(
-                                'file:preprocessor',
-                                wp({
-                                    webpackOptions: {
-                                        resolve: {
-                                            modules: [
-                                                'node_modules',
-                                                globalCypressPath,
-                                            ].filter(Boolean),
-                                        },
-                                    },
-                                })
-                            );
-
-                            // Run any custom setupNodeEvents logic passed by the user
-                            if (typeof originalSetupNodeEvents === 'function') {
-                                return originalSetupNodeEvents(on, config);
-                            }
-                        },
-                    },
-                });
-            };
-
-            loadedModule._isPatched = true;
+            }
         }
     }
 
